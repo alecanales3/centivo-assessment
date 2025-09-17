16e44d0b89acb669dcfe912522b584642cba4541
Alejandro C. <aacanales3@gmail.com>
2025-09-16 21:36:56 -0500

    Add mongoose integration

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+
 const app = express();
 
-app.get("/users/:id", (req, res) => {
-  res.json({ message: `User ID requested: ${req.params.id}` });
+mongoose.connect("mongodb://localhost:27017/centivo-db");
+
+app.get("/users/:id", async (req, res) => {
+  const user = await User.findOne({ _id: req.params.id, age: { $gt: 21 } });
+  if (!user) return res.status(404).json({ error: "User not found or age <= 21" });
+  res.json(user);
 });
 
 const PORT = 5000;
