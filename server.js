9066a72a7aaf57bbc2d15e54dea544081ca0b87a
Alejandro C. <aacanales3@gmail.com>
2025-09-16 21:14:40 -0500

    Add users by id API

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/users/:id", (req, res) => {
+  res.json({ message: `User ID requested: ${req.params.id}` });
+});
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
